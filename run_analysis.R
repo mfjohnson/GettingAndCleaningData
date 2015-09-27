57abd54e6bbc4bb92399bdbca5589d1ca3584d13
Mark Johnson <mjohnson@HW10424.local>
2015-09-27 19:09:13 -0400

    Re arranged the steps to more closely align with the project specified steps.

--- run_analysis.R
+++ run_analysis.R
@@ -5,14 +5,24 @@
 setwd("~/R_Coursera_Course/gettingAndCleaning/UCI HAR Dataset")
 
 library(plyr)
+library(dplyr)
 
 # Step 0: Load the UCI HAR Dataset files and specify the column names and column types
-X_test <- read.table("test/X_test.txt")
+features <-
+  read.table(
+    "features.txt", colClasses = c("numeric","factor"), col.names = c("colId","featureLabel")
+  )
+features$featureLabel <- sapply(features$featureLabel, function(x) sub("[[:punct:]][[:punct:]][[:punct:]]","",x))
+features$featureLabel <- sapply(features$featureLabel, function(x) sub("[[:punct:]]","",x))
+featureLabels <- as.vector(features$featureLabel)
+
+
+X_test <- read.table("test/X_test.txt", col.names=featureLabels)
 Y_test <-
   read.table(
     "test/y_test.txt", colClasses = c("factor"), col.names = c("activityId")
   )
-X_train <- read.table("train/X_train.txt")
+X_train <- read.table("train/X_train.txt", col.names = featureLabels)
 Y_train <-
   read.table(
     "train/y_train.txt", colClasses = c("factor"), col.names = c("activityId")
@@ -30,60 +40,48 @@ subject_Train <-
 X_train$DataType <- "TRAIN"
 X_test$DataType <- "TEST"
 
+
 # Load the support data frames (features, subject_test and activity Labels)
-features <-
-  read.table(
-    "features.txt", colClasses = c("numeric","factor"), col.names = c("colId","featureLabel")
-  )
 activity_labels <-
   read.table(
     "activity_labels.txt", colClasses = c("numeric","factor"), col.names = c("activityId","activityLabel")
   )
 
-
-# Step #1: Appropriately labels the TRAIN and TEST data sets with descriptive variable names. (Note: Listed as
-#         step #4 in the original instrucitons.  Placed at the front of the processing cycle to
-#         trim down the data set early for performance advantages.)
-names(X_train) <- as.vector(features$featureLabel)
-names(X_test) <- as.vector(features$featureLabel)
+# Step #1: Merge the trainng and test sets to create on Dataset
+tiddyData <-
+  rbind(cbind(subject_Test, Y_test, X_test), cbind(subject_Train, Y_train, X_train))
 
 # Step #2: Extract only the measurements on the mean and standard deviation for each measurement.
 #         This step reduces the working dataset to just the principlal fields required for the
 #         assignment.
-focusFeatures <-
-  subset(features, (!grepl(
-    "meanfreq", tolower(features$featureLabel)
-  )
-  &
-    !grepl("angle", tolower(
-      features$featureLabel
-    )))
-  &
-    (
-      grepl("mean", tolower(features$featureLabel)) |
-        grepl("std", tolower(features$featureLabel)) |
-        grepl("ActivityId", tolower(features$featureLabel)) |
-        grepl("SubjectId", tolower(features$featureLabel))
-    ))
+tiddyDataFieldNames <- data.frame(featureLabel = names(tiddyData), colId = 1:ncol(tiddyData))
+#tiddyDataFieldNames$colId <- ifelse(tiddyDataFieldNames$featureLabel=='subjectId',)
+#tiddyDataFieldNames <- rbind(c("1","SubjectId"), focusedFeatures)
+#tiddyDataFieldNames <- rbind(c("2","activityId"), focusedFeatures)
+focusedFeatures <- tiddyDataFieldNames %>%  filter(
+  grepl("mean", featureLabel) |
+    grepl("std", featureLabel) |
+    grepl("activityId", featureLabel) |
+    grepl("subjectId", featureLabel)
+)
 
-xTrain <- X_train[,c(focusFeatures$colId)]
-xTest <- X_test[,c(focusFeatures$colId)]
-rm(X_train)
-rm(X_test)
+tiddyData <- subset(tiddyData, select = c(focusedFeatures$colId))
 
-# Step #3: Merge the trainng and test sets to create on Dataset
-tiddyData <-
-  rbind(cbind(subject_Test, Y_test, xTest), cbind(subject_Train, Y_train, xTrain))
 
-# Step #4: Name the activites in the dataset
+
+# Step #3: Uses descriptive activity names to name the activities in the data set
 tiddyData$activityLabel <-
   activity_labels[tiddyData$activityId,]$activityLabel
 rm(Y_test)
 rm(subject_Test)
 rm(subject_Train)
+rm(focusedFeatures)
+rm(tiddyDataFieldNames)
 rm(Y_train)
 
+# Step #4: Appropriately labels the data set with descriptive variable names. 
 
+#TODO
 # Step #5: From the data set in step 4, creates a second, independent tidy data set with the
 #          average of each variable for each activity and each subject.
 tiddyMean <-
